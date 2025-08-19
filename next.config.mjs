e533a0634fabd02236cb3b59a70a68676aa1b197
David Fahlander <david.fahlander@gmail.com>
2025-08-19 09:16:19 +0200

    Workaround for `yjs already imported`

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from 'path'
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack },
+  ) => {
+    // This is a workaround for the issue with yjs not being able to resolve
+    // the correct path for the yjs module in Next.js.
+    // See:
+    //  * https://github.com/yjs/yjs/issues/438
+    //  * https://github.com/facebook/lexical/issues/3113
+    config.resolve.alias.yjs = path.resolve('node_modules/yjs/dist/yjs.cjs')
+    return config
+  },
+}
+
+export default nextConfig
